c42fd88b55b0e270627895b3b20cf8bc6c534b8f
mabyoungg <mabyoungg@gmail.com>
2024-05-22 19:15:30 +0900

    feat: openapi-typescript 적용

--- src/routes/p/list/+page.svelte
+++ src/routes/p/list/+page.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
+	import type { components } from '$lib/types/api/v1/schema';
+
 	async function load() {
-		return await fetch('http://localhost:8090/api/v1/posts').then((res) => res.json());
+		return (await fetch('http://localhost:8090/api/v1/posts').then((res) =>
+			res.json()
+		)) as components['schemas']['RsDataGetPostsResponseBody'];
 	}
 </script>
 
 {#await load()}
 	<p>loading...</p>
-{:then data}
+{:then { data: { items: posts } }}
 	<h1>글 리스트</h1>
 	<ul>
-		{#each data.data.items as post}
+		{#each posts as post}
 			<li>
 				<a href="/p/{post.id}">{post.title}</a>
 			</li>

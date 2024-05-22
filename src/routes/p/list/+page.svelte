4ddf2270768d4d8774ab1f5101dfa3d692c53878
mabyoungg <mabyoungg@gmail.com>
2024-05-22 18:55:00 +0900

    feat: 글 목록 페이지

---
+++ src/routes/p/list/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	async function load() {
+		return await fetch('http://localhost:8090/api/v1/posts').then((res) => res.json());
+	}
+</script>
+
+{#await load()}
+	<p>loading...</p>
+{:then data}
+	<h1>글 리스트</h1>
+	<ul>
+		{#each data.data.items as post}
+			<li>
+				<a href="/p/{post.id}">{post.title}</a>
+			</li>
+		{/each}
+	</ul>
+{/await}

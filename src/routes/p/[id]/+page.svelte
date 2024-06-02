ce13b981d9bf05ac6e6cbfe30482474f9cf89a34
mabyoungg <mabyoungg@gmail.com>
2024-06-02 20:10:34 +0900

    refactor: 글, 댓글 로딩 함수 분리

--- src/routes/p/[id]/+page.svelte
+++ src/routes/p/[id]/+page.svelte
@@ -4,43 +4,72 @@
 	import ToastUiEditor from '$lib/components/ToastUiEditor.svelte';
 	import { prettyDateTime } from '$lib/utils';
 
-	async function load() {
+	async function loadPost() {
 		if (import.meta.env.SSR) throw new Error('CSR ONLY');
 
-		const [postData, commentsData] = await Promise.all([
-			rq
-				.apiEndPoints()
-				.GET('/api/v1/posts/{id}', { params: { path: { id: parseInt($page.params.id) } } }),
-			rq.apiEndPoints().GET('/api/v1/postComments/{postId}', {
-				params: { path: { postId: parseInt($page.params.id) } }
-			})
-		]);
+		const { data, error } = await rq
+			.apiEndPoints()
+			.GET('/api/v1/posts/{id}', { params: { path: { id: parseInt($page.params.id) } } });
 
-		if (postData.error) throw postData.error;
-		if (commentsData.error) throw commentsData.error;
+		if (error) throw error;
 
-		return { postData: postData.data!, commentsData: commentsData.data! };
+		return data!;
+	}
+
+	async function loadPostComments() {
+		if (import.meta.env.SSR) throw new Error('CSR ONLY');
+
+		const { data, error } = await rq.apiEndPoints().GET('/api/v1/postComments/{postId}', {
+			params: { path: { postId: parseInt($page.params.id) } }
+		});
+		if (error) throw error;
+
+		return data!;
 	}
 </script>
 
-{#await load()}
+{#await loadPost()}
 	<div>loading...</div>
-{:then { postData: { data: { item: post } }, commentsData: { data: { items: postComments } } }}
-	<h1>{post.title}</h1>
+{:then { data: { item: post } }}
+	<h1>번호 : {post.id}</h1>
+	<h1>제목 : {post.title}</h1>
+	<div>작성 : {prettyDateTime(post.createDate)}</div>
+	<div>수정 : {prettyDateTime(post.modifyDate)}</div>
+	<div>작성자 : {post.authorName}</div>
+	<div>
+		<img src={post.authorProfileImgUrl} width="30" class="rounded-full" alt="" />
+	</div>
 	<div>추천 : {post.likesCount}</div>
 
 	{#key post.id}
 		<ToastUiEditor body={post.body} viewer={true} />
 	{/key}
 
+	<div>
+		{#if post.actorCanDelete}
+			<button onclick={() => rq.confirmAndDeletePost(post, '/p/list')}>삭제</button>
+		{/if}
+
+		{#if post.actorCanEdit}
+			<a href="/p/{post.id}/edit">수정</a>
+		{/if}
+	</div>
+{:catch error}
+	{error.msg}
+{/await}
+
+{#await loadPostComments()}
+	<div>loading...</div>
+{:then { data: { items: postComments } }}
 	<h1 class="font-bold text-2xl">댓글</h1>
 
 	<div>
 		{#each postComments as postComment}
 			<div>
-				<div>{postComment.id}</div>
-				<div>{prettyDateTime(postComment.createDate)}</div>
-				<div>{postComment.authorName}</div>
+				<div>번호 : {postComment.id}</div>
+				<div>작성 : {prettyDateTime(postComment.createDate)}</div>
+				<div>수정 : {prettyDateTime(postComment.modifyDate)}</div>
+				<div>작성자 : {postComment.authorName}</div>
 				<div>
 					<img src={postComment.authorProfileImgUrl} width="30" class="rounded-full" alt="" />
 				</div>
@@ -51,16 +80,6 @@
 				</div>
 			</div>
 		{/each}
-	</div>
-
-	<div>
-		{#if post.actorCanDelete}
-			<button onclick={() => rq.confirmAndDeletePost(post, '/p/list')}>삭제</button>
-		{/if}
-
-		{#if post.actorCanEdit}
-			<a href="/p/{post.id}/edit">수정</a>
-		{/if}
 	</div>
 {:catch error}
 	{error.msg}

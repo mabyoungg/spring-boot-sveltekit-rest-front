b9e6d6b9b659c77c66ff0aa581492bdb6ccc73b0
mabyoungg <mabyoungg@gmail.com>
2024-06-09 19:50:38 +0900

    style: 메인화면

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,19 @@
-<div>메인</div>
+<div class="flex-grow flex justify-center items-center">
+	<div>
+		<div class="text-center">
+			<div class="font-bold text-lg">테스트 블로그</div>
+			<div class="mt-3 text-gray-400">에디터와 뷰어 제공</div>
+		</div>
+
+		<div class="divider"></div>
+
+		<div>
+			<div class="font-bold text-lg text-center">공지사항</div>
+
+			<ul class="list-disc m-5 text-gray-400">
+				<li>공지사항 1</li>
+				<li>공지사항 2</li>
+			</ul>
+		</div>
+	</div>
+</div>

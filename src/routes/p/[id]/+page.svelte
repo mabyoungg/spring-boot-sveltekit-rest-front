44e570e17d80eeba0a9d5cbfbe3027a209e892ef
mabyoungg <mabyoungg@gmail.com>
2024-06-11 16:31:44 +0900

    style: 글 상세 페이지

--- src/routes/p/[id]/+page.svelte
+++ src/routes/p/[id]/+page.svelte
@@ -184,143 +184,189 @@
 	});
 </script>
 
-{#await loadPost()}
-	<div>loading...</div>
-{:then { data: { item: post } }}
-	<h1>번호 : {post.id}</h1>
-	<h1>제목 : {post.title}</h1>
-	<div>작성 : {prettyDateTime(post.createDate)}</div>
-	<div>수정 : {prettyDateTime(post.modifyDate)}</div>
-	<div>작성자 : {post.authorName}</div>
-	<div>
-		<img src={post.authorProfileImgUrl} width="30" class="rounded-full" alt="" />
-	</div>
-	<div>추천 : {post.likesCount}</div>
+<div class="flex-grow flex justify-center items-center">
+	<div class="w-full px-2 mt-4">
+		{#await loadPost()}
+			<div>loading...</div>
+		{:then { data: { item: post } }}
+			<div class="card bg-base-100 shadow">
+				<div class="card-body">
+					<div class="detail grid grid-cols-[repeat(auto-fit,minmax(100px,1fr))] gap-3">
+						<div class="form-control">
+							<label class="label">
+								<span class="label-text">번호</span>
+							</label>
+							<div>{post.id}</div>
+						</div>
 
-	{#key post.id}
-		<ToastUiEditor bind:this={toastUiEditor} body={post.body} viewer={true}>
-			<div slot="beforeContent">
-				<!-- svelte-ignore a11y_click_events_have_key_events -->
-				<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
-				<h1>
-					제목 : {post.title},
-					<div class="tooltip tooltip-right" data-tip="Ctrl + d, Cmd + d">
-						<button class="btn btn-sm" on:click={() => toastUiEditor.toggleFullScreen()}
-							>전체화면</button
-						>
+						<div class="form-control">
+							<label class="label">
+								<span class="label-text">작성일</span>
+							</label>
+							<div>{prettyDateTime(post.createDate)}</div>
+						</div>
+
+						<div class="form-control">
+							<label class="label">
+								<span class="label-text">작성자</span>
+							</label>
+							<div>{post.authorName}</div>
+						</div>
+
+						<div class="form-control">
+							<label class="label">
+								<span class="label-text">공개여부</span>
+							</label>
+							{#if post.published}
+								<i class="fa-regular fa-eye"></i>
+							{:else}
+								<i class="fa-regular fa-eye-slash"></i>
+							{/if}
+						</div>
+
+						<div class="form-control col-span-full">
+							<label class="label">
+								<span class="label-text">제목</span>
+							</label>
+							<div>{post.title}</div>
+						</div>
+
+						<div class="form-control col-span-full">
+							<label class="label">
+								<span class="label-text">내용</span>
+							</label>
+
+							{#key post.id}
+								<ToastUiEditor bind:this={toastUiEditor} body={post.body} viewer={true}>
+									<div slot="beforeContent">
+										<!-- svelte-ignore a11y_click_events_have_key_events -->
+										<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+										<h1>
+											제목 : {post.title},
+											<div class="tooltip tooltip-right" data-tip="Ctrl + d, Cmd + d">
+												<button class="btn btn-sm" on:click={() => toastUiEditor.toggleFullScreen()}
+													>전체화면</button
+												>
+											</div>
+										</h1>
+									</div>
+								</ToastUiEditor>
+							{/key}
+						</div>
 					</div>
-				</h1>
-			</div>
-		</ToastUiEditor>
-	{/key}
-
-	<div>
-		{#if post.actorCanDelete}
-			<button on:click={() => rq.confirmAndDeletePost(post, '/p/list')}>삭제</button>
-		{/if}
-
-		{#if post.actorCanEdit}
-			<a href="/p/{post.id}/edit">수정</a>
-		{/if}
-	</div>
-{:catch error}
-	{error.msg}
-{/await}
-
-<div>
-	<h1 class="font-bold text-2xl">댓글작성</h1>
-
-	<form on:submit|preventDefault={submitWriteCommentForm}>
-		{#if tempPostCommentId == 0}
-			<input type="text" class="input input-bordered" on:click={() => makeTempPostComment()} />
-		{/if}
-
-		{#if tempPostCommentId > 0}
-			<div>
-				<div>내용</div>
-				{#key tempPostCommentId}
-					<ToastUiEditor
-						bind:this={writeCommentToastUiEditor}
-						body={''}
-						saveBody={() => submitWriteCommentForm()}
-					/>
-				{/key}
-			</div>
-
-			<div>
-				<button type="submit">작성</button>
-			</div>
-		{/if}
-	</form>
-</div>
-
-{#await loadPostComments()}
-	<div>loading...</div>
-{:then { }}
-	<h1 class="font-bold text-2xl">댓글</h1>
-
-	<div>
-		{#each postComments as postComment}
-			<div class="border">
-				<div>번호 : {postComment.id}</div>
-				<div>작성 : {prettyDateTime(postComment.createDate)}</div>
-				<div>수정 : {prettyDateTime(postComment.modifyDate)}</div>
-				<div>작성자 : {postComment.authorName}</div>
-				<div>
-					<img src={postComment.authorProfileImgUrl} width="30" class="rounded-full" alt="" />
 				</div>
-				{#if !postComment.editing}
+			</div>
+
+			<div>
+				{#if post.actorCanDelete}
+					<button on:click={() => rq.confirmAndDeletePost(post, '/p/list')}>삭제</button>
+				{/if}
+
+				{#if post.actorCanEdit}
+					<a href="/p/{post.id}/edit">수정</a>
+				{/if}
+			</div>
+		{:catch error}
+			{error.msg}
+		{/await}
+
+		<div>
+			<h1 class="font-bold text-2xl">댓글작성</h1>
+
+			<form on:submit|preventDefault={submitWriteCommentForm}>
+				{#if tempPostCommentId == 0}
+					<input type="text" class="input input-bordered" on:click={() => makeTempPostComment()} />
+				{/if}
+
+				{#if tempPostCommentId > 0}
 					<div>
-						{#key postComment.id}
-							<ToastUiEditor body={postComment.body} viewer={true} />
+						<div>내용</div>
+						{#key tempPostCommentId}
+							<ToastUiEditor
+								bind:this={writeCommentToastUiEditor}
+								body={''}
+								saveBody={() => submitWriteCommentForm()}
+							/>
 						{/key}
 					</div>
 
 					<div>
-						{#if postComment.actorCanDelete}
-							<button
-								on:click={() =>
-									confirmAndDeletePostComment(postComment, (data) => {
-										rq.msgInfo(data.msg);
-										postComments.splice(postComments.indexOf(postComment), 1);
-									})}>삭제</button
-							>
-						{/if}
-
-						{#if postComment.actorCanEdit}
-							<button on:click={() => (postComment.editing = !postComment.editing)}>수정</button>
-						{/if}
+						<button type="submit">작성</button>
 					</div>
 				{/if}
+			</form>
+		</div>
 
-				{#if postComment.editing}
-					<div>
-						<form on:submit|preventDefault={() => submitEditCommentForm(postComment.id)}>
-							<input type="hidden" name="id" value={postComment.id} />
+		{#await loadPostComments()}
+			<div>loading...</div>
+		{:then { }}
+			<h1 class="font-bold text-2xl">댓글</h1>
 
+			<div>
+				{#each postComments as postComment}
+					<div class="border">
+						<div>번호 : {postComment.id}</div>
+						<div>작성 : {prettyDateTime(postComment.createDate)}</div>
+						<div>수정 : {prettyDateTime(postComment.modifyDate)}</div>
+						<div>작성자 : {postComment.authorName}</div>
+						<div>
+							<img src={postComment.authorProfileImgUrl} width="30" class="rounded-full" alt="" />
+						</div>
+
+						{#if !postComment.editing}
 							<div>
-								<div>내용</div>
 								{#key postComment.id}
-									<ToastUiEditor
-										bind:this={toastUiEditors[postComment.id]}
-										body={postComment.body}
-										saveBody={() => submitEditCommentForm(postComment.id)}
-									/>
+									<ToastUiEditor body={postComment.body} viewer={true} />
 								{/key}
 							</div>
 
 							<div>
-								<button type="submit">수정</button>
-								<button type="button" on:click={() => (postComment.editing = !postComment.editing)}
-									>수정취소</button
-								>
+								{#if postComment.actorCanDelete}
+									<button
+										on:click={() =>
+											confirmAndDeletePostComment(postComment, (data) => {
+												rq.msgInfo(data.msg);
+												postComments.splice(postComments.indexOf(postComment), 1);
+											})}>삭제</button
+									>
+								{/if}
+
+								{#if postComment.actorCanEdit}
+									<button on:click={() => (postComment.editing = !postComment.editing)}>수정</button
+									>
+								{/if}
 							</div>
-						</form>
+						{/if}
+
+						{#if postComment.editing}
+							<div>
+								<form on:submit|preventDefault={() => submitEditCommentForm(postComment.id)}>
+									<input type="hidden" name="id" value={postComment.id} />
+
+									<div>
+										<div>내용</div>
+										{#key postComment.id}
+											<ToastUiEditor
+												bind:this={toastUiEditors[postComment.id]}
+												body={postComment.body}
+												saveBody={() => submitEditCommentForm(postComment.id)}
+											/>
+										{/key}
+									</div>
+
+									<div>
+										<button type="submit">수정</button>
+										<button
+											type="button"
+											on:click={() => (postComment.editing = !postComment.editing)}>수정취소</button
+										>
+									</div>
+								</form>
+							</div>
+						{/if}
 					</div>
-				{/if}
+				{/each}
 			</div>
-		{/each}
+		{/await}
 	</div>
-{:catch error}
-	{error.msg}
-{/await}
+</div>

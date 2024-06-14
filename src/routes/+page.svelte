7e097d85b3d39c6bc727999b12e6505adb0b4aab
mabyoungg <mabyoungg@gmail.com>
2024-06-14 18:42:25 +0900

    style: 공지사항 수정

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="flex-grow flex justify-center items-center">
-	<div class="w-full max-w-sm">
-		<div class="text-center">
+	<div class="w-full max-w-lg">
+		<div class="text-center mt-4">
 			<div class="font-bold text-lg">테스트 블로그</div>
 			<div class="mt-3 text-gray-400">에디터와 뷰어 제공</div>
 		</div>
@@ -10,9 +10,22 @@
 		<div>
 			<div class="font-bold text-lg text-center">공지사항</div>
 
-			<ul class="list-disc m-5 text-gray-400">
-				<li>공지사항 1</li>
-				<li>공지사항 2</li>
+			<ul class="list-disc my-4 ml-7 mr-2 text-gray-400">
+				<li>공지사항 1 <i class="fa-solid fa-fire text-red-500"></i></li>
+				<li>공지사항 2 <i class="fa-solid fa-fire text-red-500"></i></li>
+				<li>
+					<span class="text-red-500">Ctrl + D</span> 글 본문페이지에서 본문 풀스크린 모드 토글
+				</li>
+				<li>
+					<span class="text-red-500">Ctrl + P</span> 에디터에서 미리보기 모드 토글
+				</li>
+				<li>
+					<span class="text-red-500">Ctrl + S</span> 에디터에서 본문(글 혹은 댓글) 저장
+				</li>
+				<li>
+					<span class="text-red-500">Ctrl + Q</span> 에디터에서 풀스크린 모드 토글
+				</li>
+				<li>본문 풀스크린 모드에서는 문서가 수정되었을 때 즉시 해당 변경사항 반영</li>
 			</ul>
 		</div>
 	</div>
